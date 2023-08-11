bb5b930cdc865f98338e012d3a497adc5f907579
leumars-o <samex150@gmail.com>
2023-08-11 22:41:08 +0100

    program 101 modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t lenght = sizeof(message) - 1;
+	fwrite(message, sizeof(char), lenght, stderr);
 	return (1);
 }
 	
