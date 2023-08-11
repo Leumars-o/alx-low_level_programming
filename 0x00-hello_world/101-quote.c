2a999e3f62eaa1ebc4ff9a33b4f2d8df7a3d3607
leumars-o <samex150@gmail.com>
2023-08-11 23:09:36 +0100

    program 101 modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * return: 1 (to stderr)
+ * Return: gives 1
  */
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t lenght = sizeof(message) - 1;
+
 	fwrite(message, sizeof(char), lenght, stderr);
+
 	return (1);
 }
-	
