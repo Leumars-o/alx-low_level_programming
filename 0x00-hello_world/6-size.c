3c6d377d45d5f741a0d9fadc24814f3524f8e16d
leumars-o <samex150@gmail.com>
2023-08-11 05:59:35 +0100

    script 6 aded

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main: entery point
+ * 
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-	::
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
