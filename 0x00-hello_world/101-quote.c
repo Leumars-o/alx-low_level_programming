9eae0f7c40c266f1eede883769a0505ade48c989
leumars-o <samex150@gmail.com>
2023-08-11 06:50:54 +0100

    program 100 and 101 added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: 1 (to stderr)
+ */
+int main(void)
+{
+	echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+	
