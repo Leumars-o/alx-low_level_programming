bf72d1487f8f6fba8c133ef02bad001ac533f0fb
leumars-o <samex150@gmail.com>
2023-08-11 05:39:34 +0100

    program-4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+  * main - function start
+  *
+  * Return: 0 (success) after printing an output
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
