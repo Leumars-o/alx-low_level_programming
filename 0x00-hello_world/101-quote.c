6b9b301a6ec8fcf675c58b7681d2a323b9d2dbba
leumars-o <samex150@gmail.com>
2023-08-11 23:12:22 +0100

    program 101 modified using betty standards

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t lenght = sizeof(message) - 1;
 
 	fwrite(message, sizeof(char), lenght, stderr);
