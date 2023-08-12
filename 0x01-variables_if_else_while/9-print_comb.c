1578742dee83b964b779c6a7946b06931eb6e2f3
leumars-o <samex150@gmail.com>
2023-08-12 09:12:59 +0100

    program 9- modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar('0' + i);
+		if (i != 9)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
