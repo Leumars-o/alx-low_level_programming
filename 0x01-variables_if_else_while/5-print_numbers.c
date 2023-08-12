509ad5c5d5970001018844cbafb83be444fa283b
leumars-o <samex150@gmail.com>
2023-08-12 06:59:34 +0100

    program that prints all single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - entry point
+  *Description:  Program that prints the digits of a number base
+  *Return: Always 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
