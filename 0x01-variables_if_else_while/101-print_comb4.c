a506da8d23f9c8bc7b7318c68c36612203325fd2
leumars-o <samex150@gmail.com>
2023-08-13 06:20:53 +0100

    program101 - prints all combination of 3 digits without repeating characters

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - Entry point
+  *
+  *Description: a programs that prints all possible combinations of 3 digits
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i != 7 || j != 8 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
