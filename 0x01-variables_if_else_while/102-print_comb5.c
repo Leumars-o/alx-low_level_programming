438b9bbe2b5699490f1fbb3008fd09f1961fbe8b
leumars-o <samex150@gmail.com>
2023-08-13 12:50:07 +0100

    program-102 - modified

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
   *main - Entry point
   *
   *Description: a programs that prints all possible combination
-  *of 2 digit numbers iterating over combination iterations
+  *of two 2-digit numbers iterating over combination iterations
   *
   *Return: Always 0
   */
@@ -13,9 +13,9 @@ int main(void)
 {
 	int m, n;
 
-	for (int n = 0; n < 99; n++)
+	for (n = 0; n < 99; n++)
 	{
-		for (int m = n + 1; m <= 99; m++)
+		for (m = n + 1; m <= 99; m++)
 		{
 			putchar('0' + n / 10);  /* Print the tens digit of n */
 			putchar('0' + n % 10);  /* Print the ones digit of n */
