72345eaffde3e0194ee7615398d953bec2d8ad87
leumars-o <samex150@gmail.com>
2023-08-16 05:45:28 +0100

    function-6 added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _abs - entry point
+  *
+  * @r: int to check
+  *
+  * Description: function that computes the absolute valur of an integer
+  *
+  * Return: int
+  */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
