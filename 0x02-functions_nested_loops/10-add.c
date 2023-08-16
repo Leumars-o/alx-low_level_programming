f91ab8e99a05eb2a26fc0547bc70a11baceb2574
leumars-o <samex150@gmail.com>
2023-08-16 18:07:59 +0100

    function-10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - entry point
+  *
+  * @i: integer parameter1
+  * @j: integer parameter2
+  *
+  * Return: Integer
+  *
+  */
+int add(int i, int j)
+{
+	i += j;
+
+	return (i);
+}
