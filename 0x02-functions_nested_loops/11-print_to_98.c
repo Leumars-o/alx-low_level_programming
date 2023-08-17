30b168514ff2d88a6922615158562d58007dd532
leumars-o <samex150@gmail.com>
2023-08-17 06:46:15 +0100

    function-11 modified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 				printf("%d, ", i);
@@ -35,5 +35,5 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 
-	} 
+	}
 }
