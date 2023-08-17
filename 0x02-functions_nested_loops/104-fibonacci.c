517d7f84f170189d7033f4d3f4a1a9f7626ecd2d
leumars-o <samex150@gmail.com>
2023-08-17 19:39:07 +0100

    program-104 added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: Program that prints the first 98 Fibonacci numbers
+  * starting with 1 and 2
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i;
+	int n = 98;
+	unsigned long fib1 = 1, fib2 = 2, sum;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%lu, ", fib1);
+		else if (i == 1)
+			printf("%lu, ", fib2);
+		else if (i > 1 && i < n)
+		{
+			sum = fib1 + fib2;
+			if (i == n - 1)
+				printf("%lu\n", sum);
+			else
+			printf("%lu, ", sum);
+			fib1 = fib2;
+			fib2 = sum;
+		}
+	}
+	return (0);
+}
+
