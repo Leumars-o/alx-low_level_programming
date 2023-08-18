4d5920e2d16333df217602fc959d8013eb4bef98
leumars-o <samex150@gmail.com>
2023-08-18 17:08:00 +0100

    function-2 added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * mul - entry point
+  *
+  * Description: function that multiplies 2 integers
+  *
+  * @a: parameter-1
+  * @b: parameter-2
+  *
+  * Return: Always 0
+  */
+int mul(int a, int b)
+{
+	int times = a * b;
+
+	return (times);
+}
