e17c7946e3b3c5db31aeb7833471ffa5363ea093
leumars-o <samex150@gmail.com>
2023-08-19 14:00:32 +0100

    function-101 modified

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,28 +13,19 @@ void print_number(int n)
 {
 	unsigned int m;
 
-	if (n < 0 && n > -1000)
+	if (n < 0)
 	{
 		m = -n;
-
 		_putchar(45);
-		if (m >= 100)
-			_putchar((m / 100) % 10 + '0');
-		if (m >= 10)
-			_putchar((m / 10) % 10 + '0');
-		_putchar((m % 10) + '0');
-
 	}
-	else if (n >= 0 && n < 10000)
+	else
 	{
 		m = n;
-
-		if (m >= 1000)
-			_putchar((m / 1000) + '0');
-		if (m >= 100)
-			_putchar((m / 100) % 10 + '0');
-		if (m >= 10)
-			_putchar((m / 10) % 10 + '0');
-		_putchar((m % 10) + '0');
 	}
+
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
 }
