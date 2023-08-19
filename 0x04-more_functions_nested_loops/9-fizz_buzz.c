0643fa38a7270234ddef485bcba916cbcf6e1b2a
leumars-o <samex150@gmail.com>
2023-08-19 05:00:47 +0100

    function-9 fizz buzz modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,18 +17,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 			continue;
 		}
 
-		if ((i % 3) == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz ");
 			continue;
 		}
-		if ((i % 5) == 0)
+		if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz ");
 			continue;
