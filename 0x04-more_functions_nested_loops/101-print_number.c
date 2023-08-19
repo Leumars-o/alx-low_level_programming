b12e73dfca18fcfd7c068c85b2525237e3f12574
leumars-o <samex150@gmail.com>
2023-08-19 04:53:23 +0100

    function-101 added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * print_number - entry point
+  *
+  * @n: parameter to be checked
+  *
+  * Description: function that prints an integer
+  *
+  * Return: Void
+  */
+void print_number(int n)
+{
+	if (n < 0 && n > -100)
+	{
+		n = -n;
+
+		_putchar(45);
+		if (n >= 10)
+		{
+			_putchar((n / 10) % 10 + '0');
+		}
+		_putchar((n % 10) + '0');
+
+	}
+	else if (n >= 0 && n < 10000)
+	{
+		if (n >= 1000)
+			_putchar((n / 1000) + '0');
+		if (n >= 100)
+			_putchar((n / 100) % 10 + '0');
+		if (n >= 10)
+			_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+}
