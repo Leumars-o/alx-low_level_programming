14e19383910e186a927d694314de6eda3c7b2e83
leumars-o <samex150@gmail.com>
2023-08-21 21:47:31 +0100

    function-0 modified using betty style

--- 0x05-pointers_array_strings/0-reset_to_98.c
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -3,6 +3,8 @@
 /**
   * reset_to_98 - entry point
   *
+  * @n: pointer to derefrence
+  *
   * Descriptin: function that takes a pointer to an int as parameter
   * and updates the value it points to 98
   *
@@ -11,4 +13,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-}	
+}
