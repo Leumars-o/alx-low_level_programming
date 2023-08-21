61a883dd1178850ea2274bc381f05fb6eb32e52d
leumars-o <samex150@gmail.com>
2023-08-21 21:56:52 +0100

    function-1 added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * swap_int - entry point
+  *
+  * @a: pointer a
+  * @b: pointer b
+  *
+  * Description: function that swaps the values of two integers
+  *
+  * Return: Void
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
