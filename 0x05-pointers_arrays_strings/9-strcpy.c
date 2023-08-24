237b0ef9da26ef308c32bfe8a90746d97edd5ae2
leumars-o <samex150@gmail.com>
2023-08-24 05:59:47 +0100

    function 9- modified

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,10 +3,10 @@
 /**
   * _strcpy - entry point
   *
-  * Description: function that copies the string pointed to by 
+  * Description: function that copies the string pointed to by
   * src, including the terminating null byte (\0) to the
   * buffer pointed to by dest.
-  * 
+  *
   * @dest: pointer char parameter
   * @src: pointer char parameter
   *
