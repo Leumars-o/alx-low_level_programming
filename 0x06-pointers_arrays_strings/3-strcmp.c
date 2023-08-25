b7b71b2292f49fc18721bbe81ab44532ce7ae059
leumars-o <samex150@gmail.com>
2023-08-25 03:23:12 +0100

    function-3 modified

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,25 +8,17 @@
   * @s1: first pointer int to be compared
   * @s2: second pointer int to be compared
   *
-  * Return: int value
+  * Return: int value s1[i] - s2[i]
   */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; *(s2 + i) != '\0'; i++)
+	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 	{
-		if (*(s1 + i) < *(s2 + i))
+		if (*(s1 + i) != *(s2 + i))
 		{
-			return (-1);
-		}
-		else if (*(s1 + i) == *(s2 + i))
-		{
-			continue;
-		}
-		else if (*(s1 + i) > *(s2 + i))
-		{
-			return (1);
+			return (*(s1 + i) - *(s2 + i));
 		}
 	}
 	return (0);
