2b963e0d4d4a3a52e1fec50bb1dcdb93f634528b
leumars-o <samex150@gmail.com>
2023-09-02 00:40:53 +0100

    function-0 added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: function that prints its name
+  *
+  * @argc: arument count
+  * @argv: argument array
+  *
+  * Return: Always 0 - success
+  */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
