f448fd2032fa3612474916db83bcd2bd35f78389
leumars-o <samex150@gmail.com>
2023-09-01 05:03:25 +0100

    function-6 added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _sqrt_recursion - calculates square root
+  *
+  * Description: functiont that calculates the square root of a number
+  *
+  * @n: integer parameter
+  *
+  * Return: int value
+  */
+int _sqrt_recursion(int n)
+{
+	
