6e1fe833d279a1234869c625756933e9c1f6abe4
leumars-o <samex150@gmail.com>
2023-09-07 22:24:26 +0100

    function-101 modified: betty coding style

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,133 +1,82 @@
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
-		
+
 /**
- * count_word - helper function to count the number of words in a string
+ * count - helper function to count the number of words in a string
  *
- * @s: string to evaluate
+ * @str: string to evaluate
  *
  * Return: number of words
  */
-int count(char *s)
+int count(char *str)
 {
-	int flag, c, w;
+	int i;
+	int w = 0;
+	int len = 0;
 
-	flag = 0;
-	w = 0;
-	for (c = 0; s[c] != '\0'; c++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[c] == ' ')
-		flag = 0;
-		else if (flag == 0)
-		
-	{
-		
-																			flag = 1;
-		
-																						w++;
-		
-																								}
-		
-													}
-		
-
-		
-						return (w);
-		
-}
-		
-/**
-		
- * **strtow - splits a string into words
-		
- * @str: string to split
-		
- *
-		
- * Return: pointer to an array of strings (Success)
-		
- * or NULL (Error)
-		
- */
-		
-char **strtow(char *str)
-		
-{
-		
-	char **array, *tmp;
-		
-	int i, k = 0, len = 0, words, c = 0, start, end;
-		
-
-		
-	while (*(str + len))
-		
 		len++;
-		
-	words = count(str);
-		
-	if (words == 0)
-		
-		return (NULL);
-		
-
-		
-	array = (char **) malloc(sizeof(char *) * (words + 1));
-		
-	if (array == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i <= len; i++)
-		
-	{
-		
-		if (str[i] == ' ' || str[i] == '\0')
-		
-		{
-		
-			if (c)
-		
-			{
-		
-				end = i;
-		
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-		
-				if (tmp == NULL)
-		
-					return (NULL);
-		
-				while (start < end)
-		
-					*tmp++ = str[start++];
-		
-				*tmp = '\0';
-		
-				array[k] = tmp - c;
-		
-				k++;
-		
-				c = 0;
-		
-			}
-		
-		}
-		
-		else if (c++ == 0)
-		
-			start = i;
-		
+		if (str[i] != ' ' && str[i + 1] == ' ')
+			w++;
 	}
-		
+	return (w);
+}
+/**
+  * allocate_memory - allocates memory
+  *
+  * @len: lenght of memory to be allocated
+  *
+  * Return: Pointer char
+  */
+char *allocate_memory(int len)
+{
+	char *word = (char *)malloc(sizeof(char) * (len + 1));
 
-		
-	array[k] = NULL;
-		
+	if (word == NULL)
+		return (NULL);
+	return (word);
+}
+/**
+ * **strtow - splits a string into words
+ *
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	int i, k, word_len;
+	int words = 0;
+	int j = 0;
+	char **array, *tmp;
 
-		
+	words = count(str);
+	if (words == 0)
+		return (NULL);
+	array = (char **)malloc(sizeof(char *) * (words + 1));
+	if (array == NULL)
+		return (NULL);
+	tmp = str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (tmp != &str[i])
+			{
+				word_len = i - (tmp - str);
+				array[j] = allocate_memory(word_len);
+				if (array[j] == NULL)
+					return (NULL);
+				for (k = 0; k < word_len; k++)
+					array[j][k] = tmp[k];
+				array[j][k] = '\0';
+				j++;
+			}
+			tmp = &str[i + 1];
+		}
+	}
+	array[j] = NULL;
 	return (array);
-		
 }
