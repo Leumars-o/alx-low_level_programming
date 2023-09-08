1c179680a489b2608e8dc53d4af9b35ed876c183
leumars-o <samex150@gmail.com>
2023-09-08 01:50:42 +0100

    function-2 added

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,4 +6,22 @@
   *
   * Description: Function allocates memory for an array using malloc
   *
-  * @
+  * @nmemb: array of @size bytes each
+  * @size: size of bytes of the array
+  *
+  * Return: Pointer to the allocated memory
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	ptr = calloc(nmemb, size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
