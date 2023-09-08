eb2c663027eefc30ca90f98932c43a4d6fa2f4ac
leumars-o <samex150@gmail.com>
2023-09-08 01:33:11 +0100

    function-1 modified

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,12 @@
   * string_nconcat - function that concatenates 2 strings
   *
   * Description: Function concatenates 2 strings to a newly allocated space
-  * in memory which contain s1 followed by the first n bytes of s2 and NULL terminated
+  * in memory which contain s1 followed by the first n bytes of s2 and NULL
+  * terminated
+  *
+  * @s1: Pointer string parameter 1
+  * @s2: Pointer string parameter2
+  * @n: number of characters of s2 to concatenate
   *
   * Return: Pointer to char
   *
@@ -15,13 +20,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *_concat, *ptr;
 	unsigned int len1 = 0, len2 = 0, i = 0;
 
-	while (s1[len1])
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2[len2])
+	while (s2[len2] != '\0')
 		len2++;
 	if (n >= len2)
 		n = len2;
-	_concat = malloc(len1 + n + 1);
+	_concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (_concat == NULL)
 		return (NULL);
 	ptr = _concat;
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - Allocates memory for an array using malloc
+  *
+  * Description: Function allocates memory for an array using malloc
+  *
+  * @
