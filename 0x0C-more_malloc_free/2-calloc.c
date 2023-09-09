5c6336623152f037fd31d7c4c47f0eb40445e9dc
leumars-o <samex150@gmail.com>
2023-09-09 04:28:41 +0100

    function-2 modified

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+  * _memset - presets memory with charcter
+  *
+  * @s: memory to be set
+  * @b: char to be set into memory
+  * @n: number of times to copy @b
+  *
+  * Return: Pointer to memory
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
   * _calloc - Allocates memory for an array using malloc
   *
   * Description: Function allocates memory for an array using malloc
@@ -14,14 +33,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	unsigned int i;
 
 	ptr = calloc(nmemb, size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = 0;
-	}
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
