40781225e5189b31165ccb26d573c86dbb7b928c
leumars-o <samex150@gmail.com>
2023-09-12 04:07:24 +0100

    function-2 added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: function that prints the file name
+  *
+  * Return: int value
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
