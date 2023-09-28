d2b2697b79b29ab19cad517892b3f7055da24595
leumars-o <samex150@gmail.com>
2023-09-28 06:00:37 +0100

    function-100 added

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,39 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * free_listint_safe - functino that frees a listint_t list
-  * @head: double pointer to head of list
+  * @h: double pointer to head of list
   * Return: the size of the list that was free'd
   */
 size_t free_listint_safe(listint_t **h)
 {
-	
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (len);
+}
