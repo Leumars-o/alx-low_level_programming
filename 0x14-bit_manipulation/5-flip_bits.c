02aaccb58e69d71045e2a3dfc4c42c71f916ed45
leumars-o <samex150@gmail.com>
2023-10-27 07:17:42 +0100

    function -5 modified

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,5 +9,14 @@
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int xor_result, bit_count = 0;
 
+	xor_result = n ^ m;
+
+	while (xor_result != 0)
+	{
+		bit_count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (bit_count);
 }
