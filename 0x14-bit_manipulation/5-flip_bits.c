6e71bb77c4120a0f8ffb17475f93bdc02d6b49d0
leumars-o <samex150@gmail.com>
2023-10-27 05:59:32 +0100

    bit manipulation task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * flip_bits - function that returns the number of bits you would
+  * need to flip to get from one number to another
+  * @n: number to flip
+  * @m: number to flip to
+  * Return: number of bits taken
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
